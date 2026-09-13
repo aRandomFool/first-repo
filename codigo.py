d8aa40619084e728f7e37c7bf7ec5c02a637aee3
aRandomFool <silver.fajardo071204@gmail.com>
2026-09-13 01:27:22 -0400

    cambiando el nombre y apellido

--- codigo.py
+++ codigo.py
@@ -1,5 +1,6 @@
-nombre = "Daniel"
-apellido = "Fajardo"
+#modificando para hacer git push
+nombre = "Ernesto"
+apellido = "del Campo"
 
 saludo = f"Hola, mi nombre es {nombre} {apellido}, Como estas"
 print(saludo)
